ac3fde0dbd3135627d1f23ac7198b6a1332b8561
Jesal Patel <22754222+rubix-coder@users.noreply.github.com>
2021-09-14 14:51:36 +0530

    update

---
+++ Udemy_100_days_of_python/projects/test/main.py
@@ -0,0 +1,25 @@
+# tup = (1, 2, 3)
+# l = list(tup)
+# l.append(2)
+# print(tuple(l))
+
+# new_l[0] = list_x[-1]
+#
+list_x = [2, 3, 4, 5]
+# #n=2 list_x = [4,5,0,2,3]
+#
+#
+print(list_x)
+def rotate_element(n, list_n)
+for i in range(4):
+    list_x = list_x[-1:] + list_x[:-1]
+    print(list_x)
+#
+# # print([ele ** 2 for ele in list_x])
+# # print([True if 4 in list_x ])
+#
+# # # rotation
+# # def rotate_list(n, list_a):
+# #     for i in range(n):
+# #         for i,el in enumerate(range(-1, -len(list_x), -1)):
+# #             new_l[] = list_a[el]
