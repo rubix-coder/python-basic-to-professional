dddc2499e2e92eb032548c42b8c0efc93708d852
rubix-coder <p.jesal.work@gmail.com>
2021-09-15 10:21:16 +0530

    test O notation

--- Udemy_100_days_of_python/projects/pong-game/playground.py
+++ Udemy_100_days_of_python/projects/pong-game/playground.py
@@ -1,8 +1,24 @@
-def add(*args):
-    print(sum(args))
+import time
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
 
-
-add(2, 3, 4, )
-
-
-def calculate(**kwargs):
+data = pd.DataFrame()
+c = []
+time_list = []
+n_list = []
+for i in range(100):
+    x = [random.randint(0, 100) for i in range(random.randint(0, 10000))]
+    n_list.append(len(x))
+    start = time.time()
+    for it in x:
+        for j in range(it):
+            c.append((it ** 2) * 2 * j)
+    end = time.time()
+    time_list.append(round(abs(end - start), 2))
+    print(f"time: {end - start}")
+data = pd.DataFrame({"n_list": n_list, "time": time_list})
+data = data.sort_values(by="n_list", ascending=True, axis=0)
+plt.scatter(data.n_list, data.time)
+print(data)
+plt.show()
