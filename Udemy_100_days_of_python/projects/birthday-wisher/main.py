67e4db091c7e6273db70094631c56b7726805261
Jesal Patel <p.jesal.work@gmail.com>
2021-10-01 16:52:43 +0530

    Update main.py

--- Udemy_100_days_of_python/projects/birthday-wisher/main.py
+++ Udemy_100_days_of_python/projects/birthday-wisher/main.py
@@ -1,4 +1,4 @@
-#To run and test the code you need to update 4 places:
+# To run and test the code you need to update 4 places:
 # 1. Change MY_EMAIL/MY_PASSWORD to your own details.
 # 2. Go to your email provider and make it allow less secure apps.
 # 3. Update the SMTP ADDRESS to match your email provider.
@@ -11,26 +11,28 @@ import pandas
 import random
 import smtplib
 
-MY_EMAIL = "rubix.send@gmail.com"
+MY_EMAIL = "*****@mail.com"
 MY_PASSWORD = "*(d7ZO!Si449!QjNI"
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+birthdays_dict = {data_row["name"]: (data_row["month"], data_row["day"]) for (index, data_row) in data.iterrows()}
+
+birthday_buddies = [name for name in birthdays_dict.keys() if birthdays_dict[name] == today_tuple]
+
+for buddy in birthday_buddies:
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[NAME]", buddy)
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
+            to_addrs=birthdays_dict[buddy]["email"],
             msg=f"Subject:Happy Birthday! - testing project \n\n{contents}"
         )
