d984a3bc67b803bbd5055187479801e22aa4b541
rubix-coder <p.jesal.work@gmail.com>
2021-10-10 15:10:40 +0530

    day-27 old update

--- Udemy_100_days_of_python/projects/day-27/playground.py
+++ Udemy_100_days_of_python/projects/day-27/playground.py
@@ -23,3 +23,14 @@
 # my_car = Car(make="Nissan")
 # print(my_car.model)
 
+x = input()
+e, f, r = [int(x) for x in x.split(" ")]
+n_e = 0
+dr = 0
+x = e+f
+while x != 0:
+    x = x - r + n_e
+    n_e = 1
+    dr += 1
+print(x)
+print(dr)
